b94cc30a5378e57fb43f89fc00fbfc9de5175a9a
Kyle Norris <r.kyle.norris@gmail.com>
2025-02-26 11:10:24 -0500

    fixed typo

--- db/models.py
+++ db/models.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 class Race(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True, nullable=False)
-    description = db.Column(dbText, nullable=True)
+    description = db.Column(db.Text, nullable=True)
     ability_score_increase = db.Column(db.JSON)
     size = db.Column(db.String(10), db.CheckConstraint("size IN ('Small', 'Medium', 'Large')"))
     speed = db.Column(db.Integer)
