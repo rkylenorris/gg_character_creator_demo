03540378eaba9f5f03d601823e33e65df8aec0ca
Kyle Norris <r.kyle.norris@gmail.com>
2025-02-26 13:09:51 -0500

    added models for backgrounds and classes

--- db/models.py
+++ db/models.py
@@ -32,3 +32,56 @@ class RaceModel(db.Model):
     def __repr__(self):
         return f"Race({self.name} - {self.description})"
 
+
+class BackgroundModel(db.Model):
+    
+    __tablename__ = "Backgrounds"
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    feature = db.Column(db.JSON)
+    skill_proficiencies = db.Column(db.JSON)
+    tool_proficiencies = db.Column(db.JSON)
+    starting_equipment = db.Column(db.JSON)
+
+
+    def __init__(self, chosen_background: Background):
+        super().__init__()
+        self.name = chosen_background.name
+        self.description = chosen_background.description
+        self.feature = json.dumps(chosen_background.feature)
+        self.skill_proficiencies = json.dumps(chosen_background.skill_proficiencies)
+        self.tool_proficiencies = json.dumps(chosen_background.tool_proficiencies)
+        self.starting_equipment = json.dumps(chosen_background.equipment)
+        
+    def __repr__(self):
+        return f"Background({self.name} - {self.description})"
+
+
+class ClassModel(db.Model):
+    __tablename__ = "Classes"
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    hit_die = db.Column(db.Text)
+    primary_abilities = db.Column(db.JSON)
+    saving_throws = db.Column(db.JSON)
+    proficiencies = db.Column(db.JSON)
+    skills = db.Column(db.JSON)
+    starting_equipment = db.Column(db.JSON)
+    starting_ability_scores = db.Column(db.JSON)
+    
+    def __init__(self, chosen_class: CharacterClass):
+        super().__init__()
+        self.name = chosen_class.name
+        self.hit_die = chosen_class.hit_die
+        self.primary_abilities = json.dumps(chosen_class.primary_abilities)
+        self.saving_throws = json.dumps(chosen_class.saving_throws)
+        self.proficiencies = json.dumps(chosen_class.proficiencies)
+        self.skills = json.dumps(chosen_class.skills)
+        self.starting_equipment = json.dumps(chosen_class.starting_equipment)
+        self.starting_ability_scores = json.dumps(chosen_class.starting_ability_scores)
+    
+    def __repr__(self):
+        return f"Class({self.name} - {self.hit_die})"
