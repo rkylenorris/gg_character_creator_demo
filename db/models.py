06eb193a8902b1971c3b0d7988d5a041ba64128c
Kyle Norris <r.kyle.norris@gmail.com>
2025-02-26 13:09:15 -0500

    added models for backgrounds and classes

--- db/models.py
+++ db/models.py
@@ -1,8 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
+from character_sheet import CharacterClass, Race, Background
+import json
 
+# Initialize the database
 db = SQLAlchemy()
 
-class Race(db.Model):
+class RaceModel(db.Model):
+    
+    __tablename__ = "Races"
+    
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True, nullable=False)
     description = db.Column(db.Text, nullable=True)
@@ -11,3 +17,18 @@ class Race(db.Model):
     speed = db.Column(db.Integer)
     traits = db.Column(db.JSON)
     languages = db.Column(db.JSON)
+
+
+    def __init__(self, chosen_race: Race):
+        super().__init__()
+        self.name = chosen_race.name
+        self.description = chosen_race.description
+        self.ability_score_increase = json.dumps(chosen_race.ability_bonuses)
+        self.size = chosen_race.size
+        self.speed = chosen_race.speed
+        self.traits = json.dumps(chosen_race.traits)
+        self.languages = json.dumps(chosen_race.languages)
+    
+    def __repr__(self):
+        return f"Race({self.name} - {self.description})"
+
