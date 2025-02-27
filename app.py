06d2dd5fa9c53446a57cdc78a6927de5be67ecb0
Kyle Norris <r.kyle.norris@gmail.com>
2025-02-27 16:43:15 -0500

    created code for initializing db and populating it with classes, backgrounds, and races

--- app.py
+++ app.py
@@ -18,25 +18,25 @@ db.init_app(app)
 def home():
     return render_template('home.html')
 
-# @app.route('/create-character', methods=['GET', 'POST'])
-# def create_character():
-#     form = CharacterForm()
-#     if form.validate_on_submit():
-#         # Retrieve data from the form
-#         name = form.name.data
-#         class_name = form.char_class.data
-#         race_name = form.race.data
-#         background_name = form.background.data
-#         appearance = form.appearance.data
+@app.route('/create-character', methods=['GET', 'POST'])
+def create_character():
+    form = CharacterForm()
+    if form.validate_on_submit():
+        # Retrieve data from the form
+        name = form.name.data
+        class_name = form.char_class.data
+        race_name = form.race.data
+        background_name = form.background.data
+        appearance = form.appearance.data
         
-#         try:
-#             character = character_creator(name, class_name, race_name, background_name, appearance)
-#             return render_template('character_sheet.html', character=character)
-#         except Exception as e:
-#             error = str(e)
-#             return render_template('create_character.html', form=form, error=error)
+        try:
+            character = character_creator(name, class_name, race_name, background_name, appearance)
+            return render_template('character_sheet.html', character=character)
+        except Exception as e:
+            error = str(e)
+            return render_template('create_character.html', form=form, error=error)
     
-#     return render_template('create_character.html', form=form)
+    return render_template('create_character.html', form=form)
 
 with app.app_context():
     db.create_all()
---
+++ load_db.py
@@ -0,0 +1,51 @@
+from character_sheet import CharacterClass, Race, Background
+from app import app
+from db_tools import RaceModel, BackgroundModel, ClassModel, db
+from pathlib import Path
+import json
+
+
+
+
+def load_from_json(json_dir: str, db, app) -> None:
+    """
+    Load data from JSON files and populate the database.
+    """
+    new_models = []
+    
+    json_dir = Path(json_dir)
+    for json_file in json_dir.glob('*.json'):
+        with open(json_file, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+            
+        match json_file.stem:
+            case 'classes':
+                for class_data in data:
+                    class_obj = CharacterClass(**class_data)
+                    class_model = ClassModel(**class_obj.dict())
+                    new_models.append(class_model)
+            case 'backgrounds':
+                for background_data in data:
+                    background_obj = Background(**background_data)
+                    background_model = BackgroundModel(**background_obj.dict())
+                    new_models.append(background_model)
+            case 'races':
+                for race_data in data:
+                    race_obj = Race(**race_data)
+                    race_model = RaceModel(**race_obj.dict())
+                    new_models.append(race_model)
+            case _:
+                print(f"Unknown file: {json_file.name}")
+    
+    with app.app_context():
+        db.session.bulk_save_objects(new_models)
+        db.session.commit()
+        print("Database populated successfully.")
+        db.close()
+    
+        
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    load_from_json('data', db, app)
+    print("Database created and populated.")
