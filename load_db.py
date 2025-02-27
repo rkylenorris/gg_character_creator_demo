b77ccdde4346cc328ab432741a7d2058f0acc4c4
Kyle Norris <r.kyle.norris@gmail.com>
2025-02-27 18:36:31 -0500

    tested and loaded db with classes, races, and backgrounds

--- load_db.py
+++ load_db.py
@@ -5,8 +5,6 @@ from pathlib import Path
 import json
 
 
-
-
 def load_from_json(json_dir: str, db, app) -> None:
     """
     Load data from JSON files and populate the database.
@@ -22,17 +20,17 @@ def load_from_json(json_dir: str, db, app) -> None:
             case 'classes':
                 for class_data in data:
                     class_obj = CharacterClass(**class_data)
-                    class_model = ClassModel(**class_obj.dict())
+                    class_model = ClassModel(class_obj)
                     new_models.append(class_model)
             case 'backgrounds':
                 for background_data in data:
                     background_obj = Background(**background_data)
-                    background_model = BackgroundModel(**background_obj.dict())
+                    background_model = BackgroundModel(background_obj)
                     new_models.append(background_model)
             case 'races':
                 for race_data in data:
                     race_obj = Race(**race_data)
-                    race_model = RaceModel(**race_obj.dict())
+                    race_model = RaceModel(race_obj)
                     new_models.append(race_model)
             case _:
                 print(f"Unknown file: {json_file.name}")
@@ -41,11 +39,11 @@ def load_from_json(json_dir: str, db, app) -> None:
         db.session.bulk_save_objects(new_models)
         db.session.commit()
         print("Database populated successfully.")
-        db.close()
-    
+        
         
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     load_from_json('data', db, app)
     print("Database created and populated.")
+    db.close()
