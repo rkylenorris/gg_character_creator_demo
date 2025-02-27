755cbd15e09184b2263bdc4c0ba912148d2df136
R. Kyle Norris <r.kyle.norris@gmail.com>
2025-02-26 21:19:02 -0500

    created db tables

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from forms import CharacterForm
+from db_tools import db
 from character_sheet import character_creator  # Your character creator function
 from dotenv import load_dotenv
 import os
@@ -11,29 +12,35 @@ app.config['SECRET_KEY'] =  os.getenv('FLASK_SECRET_KEY') # Replace with a secur
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
 
+db.init_app(app)
+
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/create-character', methods=['GET', 'POST'])
-def create_character():
-    form = CharacterForm()
-    if form.validate_on_submit():
-        # Retrieve data from the form
-        name = form.name.data
-        class_name = form.char_class.data
-        race_name = form.race.data
-        background_name = form.background.data
-        appearance = form.appearance.data
+# @app.route('/create-character', methods=['GET', 'POST'])
+# def create_character():
+#     form = CharacterForm()
+#     if form.validate_on_submit():
+#         # Retrieve data from the form
+#         name = form.name.data
+#         class_name = form.char_class.data
+#         race_name = form.race.data
+#         background_name = form.background.data
+#         appearance = form.appearance.data
         
-        try:
-            character = character_creator(name, class_name, race_name, background_name, appearance)
-            return render_template('character_sheet.html', character=character)
-        except Exception as e:
-            error = str(e)
-            return render_template('create_character.html', form=form, error=error)
+#         try:
+#             character = character_creator(name, class_name, race_name, background_name, appearance)
+#             return render_template('character_sheet.html', character=character)
+#         except Exception as e:
+#             error = str(e)
+#             return render_template('create_character.html', form=form, error=error)
     
-    return render_template('create_character.html', form=form)
+#     return render_template('create_character.html', form=form)
+
+with app.app_context():
+    db.create_all()
+
 
 if __name__ == '__main__':
     app.run(debug=True)
