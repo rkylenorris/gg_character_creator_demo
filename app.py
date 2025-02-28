1f58bd010fd67a7b0f85016bb2a4cb9f657d1531
Kyle Norris <r.kyle.norris@gmail.com>
2025-02-28 15:51:10 -0500

    updated create_character to start using new form. Need to update the html page too

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 from forms import CharacterForm
 from db_tools import db
 from character_sheet import character_creator  # Your character creator function
 from dotenv import load_dotenv
 import os
+from flask_wtf.csrf import CSRFProtect
 
 load_dotenv()
 
@@ -11,16 +12,20 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] =  os.getenv('FLASK_SECRET_KEY') # Replace with a secure key in production
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
-
 db.init_app(app)
+csrf = CSRFProtect(app)
+
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
+
 @app.route('/create-character', methods=['GET', 'POST'])
 def create_character():
     form = CharacterForm()
+    form.populate_dropdowns()
+    
     if form.validate_on_submit():
         # Retrieve data from the form
         name = form.name.data
@@ -31,15 +36,13 @@ def create_character():
         
         try:
             character = character_creator(name, class_name, race_name, background_name, appearance)
+            # flash(f"Character {name} created...", "success")
             return render_template('character_sheet.html', character=character)
         except Exception as e:
             error = str(e)
             return render_template('create_character.html', form=form, error=error)
     
     return render_template('create_character.html', form=form)
-
-with app.app_context():
-    db.create_all()
 
 
 if __name__ == '__main__':
